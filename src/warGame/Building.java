731cfbe09d7cb7cdc9d4a6d17ce92078b963f5a5
Simon Kiener <kiener.simon@gmail.com>
2011-08-07 05:39:02 +0200

    actually, i added them just now...

---
+++ src/warGame/Building.java
@@ -0,0 +1,10 @@
+package warGame;
+
+/** 
+ * the Building class is used to control the Methods of the Buildings enum in City.class
+ */
+public interface Building {
+
+	public int getPrice();
+
+}
---
+++ src/warGameTests/CityTest.java
@@ -0,0 +1,26 @@
+package warGameTests;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import ch.aplu.jgamegrid.*;
+import warGame.*;
+
+
+public class CityTest {
+	
+	
+	City Bern, Basel;
+	
+	@Before
+	public void initialize() {
+		Input.setInput(new MockInput());
+		Bern = new City("Bern", new Location(20, 20));
+		Basel = new City("Basel", new Location(40, 40));
+	}
+	
+	@Test
+	public void testToString() {
+		assertEquals(Bern.toString(), "Bern");
+		assertEquals(Basel.toString(), "Basel");
+	}
+}
